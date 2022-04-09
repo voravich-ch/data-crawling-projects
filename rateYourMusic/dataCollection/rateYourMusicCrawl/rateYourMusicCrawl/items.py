38586a624dd425a27fa33d584a2c67d12d9fd182
voravich-ch <voravich.ch@gmail.com>
2022-04-09 15:22:11 +0100

    Update

--- rateYourMusic/dataCollection/rateYourMusicCrawl/rateYourMusicCrawl/items.py
+++ rateYourMusic/dataCollection/rateYourMusicCrawl/rateYourMusicCrawl/items.py
@@ -37,66 +37,6 @@ def parse_hierarchy(hierarchy):
         output.append(_dict)
     return output
 
-# def parse_hierarchy_structure(hierarchy_structure):
-#     """
-#     Parse genre hierarchy structure:
-#         Example:
-#             [
-#                 {
-#                     "genre": "Ambient Dub",
-#                     "parent_genre": "Ambient",
-#                     "top_level": "No"
-#                 },
-#             ]
-#     """
-#     # Parse html
-#     html = bs4.BeautifulSoup(hierarchy_structure, 'html.parser')
-#     # Get parent genre
-#     parent = html.find('li', class_="hierarchy_list_item hierarchy_list_item_current").get_text(strip=True)
-#     # Format data
-#     output = {}
-#     genre_list = [] # To skip sub-genres that were already parsed
-#     # Insert parent genre
-#     _parent = {
-#         "parent_genre": None,
-#         "top_level": "Yes"
-#     }
-#     output[parent] = _parent
-#     genre_list.append(parent)
-    
-#     # Iterate over all items
-#     _pass = 0 # Dummy for skipping duplicates sub-genres -- the structure is very complicated
-#     items = html.select('li > ul > li[class="hierarchy_list_item"]')
-#     for item in items:
-#         genre = item.find('a').get_text(strip=True)
-#         _dict = {
-#                 "parent_genre": [parent],
-#                 "top_level": "No"
-#             }
-#         if genre not in genre_list:
-#             output[genre] = _dict
-#             genre_list.append(genre)
-#         # Check if there are sub-sub-genres
-#         if item.find('ul'):
-#             for i in item.find_all('ul'):
-#                 if _pass == 0:
-#                     if i.find_all('ul'):
-#                         _pass = len(i.find_all('ul')) # To skip the next iterations
-#                     genre = i.find('a').get_text(strip=True)
-#                     _dict = {
-#                         "parent_genre": [item.find('a').get_text(strip=True)],
-#                         "top_level": "No"
-#                     }
-#                     if genre not in genre_list:
-#                         output[genre] = _dict
-#                         genre_list.append(genre)
-#                     else:
-#                         if item.find('a').get_text(strip=True) not in output[genre]['parent_genre']:
-#                             output[genre]['parent_genre'].append(item.find('a').get_text(strip=True))
-#                 elif _pass > 0:
-#                     _pass = _pass - 1
-#     return output
-
 def parse_n_release(i):
     try:
         return re.search(r'[0-9,]+', i).group(0)
