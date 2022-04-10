eaaa1c9299b75355cad9b340d47cb3e13ec04e9f
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-04-10 01:44:01 +0100

    Update grammyGoogleSearch.py

--- grammyAwards/grammyGoogleSearch/grammyGoogleSearch.py
+++ grammyAwards/grammyGoogleSearch/grammyGoogleSearch.py
@@ -7,6 +7,7 @@ import json
 import pandas as pd
 import requests
 import bs4
+import time
 from tqdm import tqdm
 import multiprocessing, functools
 
@@ -30,25 +31,15 @@ def construct_urls(names):
     return urls
 
 def request_data(i):
-    # Declare all global variables for this function
-    global name, role, website, description, born, died, links, query, wiki_html
-    
-    # Make a request
     response = requests.get(urls[i],
-                            proxies=proxies,
-                            verify='zyte-smartproxy-ca.crt',
-                            headers={'X-Crawlera-Debug-UA': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN) AppleWebKit/533+ (KHTML, like Gecko)',
-                                     'X-Crawlera-Region': 'GB'}
+                            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
                             )
     print(f'Status Code: {response.status_code}')
     retry = 0
     while response.status_code == 503:
         response = requests.get(urls[i],
-                                proxies=proxies,        
-                                verify='zyte-smartproxy-ca.crt',
-                                headers={'X-Crawlera-Debug-UA': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN) AppleWebKit/533+ (KHTML, like Gecko)',
-                                         'X-Crawlera-Region': 'GB'}
-                                )
+                                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
+                            )
         retry = retry + 1
         if retry == int(max_retry):
             print(f'Max retry attempt reached for: {response.url}.')
@@ -76,11 +67,8 @@ def request_data(i):
         if wiki:
             wiki_url = wiki['href']
             response = requests.get(wiki_url,
-                                    proxies=proxies,
-                                    verify='zyte-smartproxy-ca.crt',
-                                    headers={'X-Crawlera-Debug-UA': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN) AppleWebKit/533+ (KHTML, like Gecko)',
-                                             'X-Crawlera-Region': 'GB'}
-                                    )
+                                    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
+                            )
             wiki_html = str(bs4.BeautifulSoup(response.content, 'html.parser'))
         else:
             wiki_html = None
@@ -99,18 +87,12 @@ def request_data(i):
         # Insert the document to MongoDB
         collection = connect_to_city(db_name='grammyAwards', collection_name='artists_googleSearch')
         collection.insert_one(document)
+        time.sleep(5)
 
 # Parameter preparation
 collection = connect_to_city(db_name='grammyAwards', collection_name='artists')
 names = get_data(collection)
 urls = construct_urls(names)
-
-# Zyte
-proxy_host = os.environ.get('PROXY_HOST')
-proxy_port = os.environ.get('PROXY_PORT')
-proxy_auth = os.environ.get('ZYTE_SMARTPROXY_APIKEY')
-proxies = {"https": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port),
-            "http": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)}
 
 # Multiprocesing
 # Start crawling
@@ -118,7 +100,7 @@ max_retry = input('Enter max retry attempt: ')
 assert int(max_retry) >= 0, 'Max retry should be an integer equal or greater than 0'
 print('Start crawling...')
 print(f'Total pages: {len(urls)}')
-num_processors = 10
+num_processors = 1
 with multiprocessing.Pool(num_processors) as p:
     list(tqdm(p.imap(request_data, list(range(len(urls)))), total = len(urls)))
 print('Finished!')
