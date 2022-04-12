9bc6f945fae1739ca4616112d7db26fa53f43a28
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-04-12 16:16:02 +0100

    Update aboutCrawl.py

--- spotify/aboutArtist/aboutCrawl.py
+++ spotify/aboutArtist/aboutCrawl.py
@@ -27,7 +27,6 @@ class spotifyAboutCrawler:
     def start_session(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
                                        options=self.chrome_options)
-        self.driver.implicitly_wait(10)
         if self.driver:
             self.item_count_local = 0
             self.item_count_mongo = 0
@@ -64,6 +63,7 @@ class spotifyAboutCrawler:
             
     def process_web(self, url):
         self.driver.get(url)
+        self.driver.implicitly_wait(10)
         xpath = '//button[starts-with(@class, "uhDzVbFHy")]'
         button = self.driver.find_element(by=By.XPATH, value=xpath)
         self.driver.execute_script("arguments[0].click();", button)
