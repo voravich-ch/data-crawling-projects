0cfb22d7df4e9d19a9079d64751e29a2dc08f5eb
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-04-12 16:24:35 +0100

    Update grammyGoogleSearch.py

--- grammyAwards/grammyGoogleSearch/grammyGoogleSearch.py
+++ grammyAwards/grammyGoogleSearch/grammyGoogleSearch.py
@@ -35,15 +35,6 @@ def request_data(i):
                             headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
                             )
     print(f'Status Code: {response.status_code}')
-    retry = 0
-    while response.status_code == 503:
-        response = requests.get(urls[i],
-                                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
-                            )
-        retry = retry + 1
-        if retry == int(max_retry):
-            print(f'Max retry attempt reached for: {response.url}.')
-            break
     if response.status_code == 200:
         print(response.url)
         html_page = bs4.BeautifulSoup(response.content, 'html.parser')
