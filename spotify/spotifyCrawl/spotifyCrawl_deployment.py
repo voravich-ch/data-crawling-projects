9743a31289d99f2ad83af1686cac70e91e2be740
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-04-20 12:01:37 +0100

    Update spotifyCrawl_deployment.py

--- spotify/spotifyCrawl/spotifyCrawl_deployment.py
+++ spotify/spotifyCrawl/spotifyCrawl_deployment.py
@@ -25,26 +25,26 @@ def main():
     artist_ids = [i['artist_id'] for i in collection.find({}, projection = {"_id": 0, "artist_id": 1})]
     for chunk in tqdm(batch(artist_ids, 50)):
         document = crawler.request_artists_meta(chunk)
-        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='test_artist'))
+        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='artists'))
     
     # Album-level data
     for artist_id in tqdm(artist_ids):
         document = crawler.request_albums_from_artist(artist_id)
-        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='test_album'))
+        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='albums'))
     
     # Track-level data
     collection = connect_to_db(db_name='spotify', collection_name='test_album')
     album_ids = [i['id'] for i in collection.find({}, projection = {"_id": 0, "id": 1})]
     for album_id in tqdm(album_ids):
         document = crawler.request_tracks_from_album(album_id)
-        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='test_track'))
+        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='tracks'))
     
     # Acoustic attributes
     collection = connect_to_db(db_name='spotify', collection_name='test_track')
     track_ids = [i['id'] for i in collection.find({}, projection = {"_id": 0, "id": 1})]
     for chunk in tqdm(batch(track_ids, 100)):
         document = crawler.request_acoustic_attributes(track_ids)
-        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='test_acoustic_attributes'))
+        insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='acousticAttributes'))
     
 if __name__ == "__main__":
     main()
