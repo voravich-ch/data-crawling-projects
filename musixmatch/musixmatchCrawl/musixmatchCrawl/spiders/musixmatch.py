5f820ff422034e4a030037ab60f2c6b5fc49ae30
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-05-05 13:26:33 +0100

    Update musixmatch.py

--- musixmatch/musixmatchCrawl/musixmatchCrawl/spiders/musixmatch.py
+++ musixmatch/musixmatchCrawl/musixmatchCrawl/spiders/musixmatch.py
@@ -1,37 +1,48 @@
-#!usr/bin/env python3
+import scrapy
+from musixmatchCrawl.items import musixmatchItem
+from scrapy.loader import ItemLoader
+import datetime
 
-import os
-import re
-import pymongo
-import pandas as pd
-from tqdm import tqdm
+def populate_url():
+    # Specify the base url
+    base_url = 'https://www.musixmatch.com/track'
+    # Specify min-max track ids
+    # range = [555,813, 233,106,400 ]
+    min_track = 555813
+    # max_track = 233,106,400 
+    max_track= 240000000
+    # Create a range of track ids
+    tracks = range(min_track, max_track + 1)
+    # Populate urls
+    urls = [f'{base_url}/{track}' for track in tracks]
+    # Define spider's name
+    spd_name = f'{min_track}_{max_track}'
+    return urls, spd_name
 
-def connect_to_city(db_name, collection_name):
-    client = pymongo.MongoClient()
-    db = client[db_name]
-    collection = db[collection_name]
-    return collection
+urls, spd_name = populate_url()
 
-def load_and_insert_data(f_name, collection):
-    chunks = pd.read_json(f_name, orient='records', lines=True, chunksize=20000)
-    for df in tqdm(chunks):
-        # Remove duplicates (Obsolete track ids)
-        # row_select = df.apply(lambda i: i['variables']['track']['id'] == int(re.search('\d+', i['request_url']).group(0)), axis=1)
-        # -- vectorized approach above is efficient but not for this case because some records were not properly formatted
-        for i in tqdm(range(len(df))):
-            try:
-                check = df.iloc[i]['variables']['track']['id'] == int(re.search('\d+', df.iloc[i]['request_url']).group(0))
-                if check:
-                    # Insert to Mongo
-                    document = df.iloc[i].to_dict()
-                    collection.insert_one(document)
-            except:
-                pass
-    os.rename(f_name, f'up_{f_name}')
+# Setting up XPath
+var_xpath = '/html/body/script[1]/text()'
 
-def main():
-    collection = connect_to_city(db_name='musixmatch', collection_name='data')
-    load_and_insert_data(f_name='555813_240000000.jl', collection=collection)
+class musixmatchSpider(scrapy.Spider):
+    name = spd_name
+    allowed_domains = ['musixmatch.com']
+    start_urls = urls
     
-if __name__ == "__main__":
-    main()
+    def parse(self, response):
+        # Create loader
+        l = ItemLoader(item=musixmatchItem(), response=response)
+
+        # Primary fields
+        l.add_xpath('variables', var_xpath)
+
+        # Housekeeping fields
+        l.add_value('request_url', response.request.meta['redirect_urls'])
+        l.add_value('response_url', response.url)
+        l.add_value('spider', self.name)
+        l.add_value('crawl_date', datetime.datetime.now().strftime('%d/%m/%Y'))
+
+        # print log
+        print(self.crawler.stats.get_stats())
+
+        return l.load_item()
