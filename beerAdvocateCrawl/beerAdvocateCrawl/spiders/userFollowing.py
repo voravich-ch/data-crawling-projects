0ec3eeb6c947127c1283ad15051a123c29b3d361
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-05-13 16:53:08 +0100

    Update userFollowing.py

--- beerAdvocateCrawl/beerAdvocateCrawl/spiders/userFollowing.py
+++ beerAdvocateCrawl/beerAdvocateCrawl/spiders/userFollowing.py
@@ -39,8 +39,7 @@ class UserFollowingSpider(scrapy.Spider):
         return FormRequest.from_response(response,
                                          formdata={"login": os.environ.get('user'),
                                                    "password": os.environ.get('password')},
-                                         callback=self.redirect
-        )
+                                         callback=self.redirect)
     
     # Redirect to the page after login
     def redirect(self, response):
