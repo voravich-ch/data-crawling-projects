627e606524b600e28ba2cf8593cb97e7b82cc573
Voravich Chaturavichanan <voravich.ch@gmail.com>
2022-08-01 16:39:08 +0100

    Update spotifyCrawl_deployment.py

--- spotify/spotifyCrawl/spotifyCrawl_deployment.py
+++ spotify/spotifyCrawl/spotifyCrawl_deployment.py
@@ -9,15 +9,17 @@ This script utilises Spotify Artist ID to crawl the following data from Spotify:
     - Acoustic Attributes (Audio Features)
 """
 
-from spotifyCrawl import spotifyCrawler, connect_to_db, insert_data_to_mongo, batch
+from spotifyCrawl import *
 from dotenv import load_dotenv
 from tqdm import tqdm
+import random
+import time
 
 # Load credentials
 load_dotenv()
 
 def main():
-    # Initialise spotifyCrawler
+    # Initialise spotifyCrawlers
     crawler = spotifyCrawler(_id=os.environ.get('_id'), _secret=os.environ.get('_secret'))
     
     # Artist-level data
@@ -26,11 +28,15 @@ def main():
     for chunk in tqdm(batch(artist_ids, 50)):
         document = crawler.request_artists_meta(chunk)
         insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='artists'))
+        time.sleep(random.randint(0, 3))
+    print("Artist-level data completed.")
     
     # Album-level data
     for artist_id in tqdm(artist_ids):
         document = crawler.request_albums_from_artist(artist_id)
         insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='albums'))
+        time.sleep(random.randint(0, 3))
+    print("Album-level data completed.")
     
     # Track-level data
     collection = connect_to_db(db_name='spotify', collection_name='test_album')
@@ -38,6 +44,8 @@ def main():
     for album_id in tqdm(album_ids):
         document = crawler.request_tracks_from_album(album_id)
         insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='tracks'))
+        time.sleep(random.randint(0, 3))
+    print("Track-level data completed.")
     
     # Acoustic attributes
     collection = connect_to_db(db_name='spotify', collection_name='test_track')
@@ -45,6 +53,8 @@ def main():
     for chunk in tqdm(batch(track_ids, 100)):
         document = crawler.request_acoustic_attributes(chunk)
         insert_data_to_mongo(data=document, collection=connect_to_db(db_name='spotify', collection_name='acousticAttributes'))
+        time.sleep(random.randint(0, 3))
+    print("Acoustic attributes data completed.")
     
 if __name__ == "__main__":
     main()
